c4a4cf782cd4f7619a79ecf1df24a4f940dedfcc
lonnen <chris.lonnen@gmail.com>
2020-12-26 03:05:42 +0000

    add our custom error and result
    this module introduces a custom error and result. As the VM develops
    we can add new errors to the enum and use thiserror macros to do the
    bookkeeping

---
+++ src/error.rs
@@ -0,0 +1,12 @@
+#[derive(thiserror::Error)]
+pub enum Error {
+
+}
+
+impl std::fmt::Debug for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
